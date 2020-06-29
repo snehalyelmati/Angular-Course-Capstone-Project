50ef5f648b209911a6b4c3a6a79ae4f073adfcfa
snehalreddy <syelmat@gmail.com>
2020-06-29 15:26:14 +0530

    Basic navigation done.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'CapstoneProject';
+  currentRoute = 'recipes'; // available options: recipes or shopping-list
+
+  onRouteChange(route: string) {
+    this.currentRoute = route;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() route = new EventEmitter<string>();
 
   constructor() {
   }
@@ -13,4 +14,7 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRouteChange(changeTo: string) {
+    this.route.emit(changeTo);
+  }
 }
