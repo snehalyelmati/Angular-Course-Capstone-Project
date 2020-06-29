5441db65767ab1b9216b0c0a40f2c395ca432002
snehalreddy <syelmat@gmail.com>
2020-06-30 00:53:22 +0530

    Added ingredients to recipes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'CapstoneProject';
-  currentRoute = 'shopping-list'; // available options: recipes or shopping-list
+  currentRoute = 'recipes'; // available options: recipes or shopping-list
 
   onRouteChange(route: string) {
     this.currentRoute = route;
