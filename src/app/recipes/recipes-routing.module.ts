6d5a47e077c85db6411bf93b850646d297d66b88
snehalreddy <syelmat@gmail.com>
2020-07-18 00:03:27 +0530

    Lazy loading and preloading features implemented.

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -9,7 +9,7 @@ import {RecipesResolverService} from './recipes-resolver.service';
 
 const routes: Routes = [
   {
-    path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard], children: [
+    path: '', component: RecipesComponent, canActivate: [AuthGuard], children: [
       {path: '', pathMatch: 'full', component: EmptyRecipeComponent},
       {path: 'new', component: RecipeEditComponent},
       {path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService]},
